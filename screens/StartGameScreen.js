42e898e253b47d31f758e7ab879843107a9a05ea
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-02-13 19:43:16 +0700

    + confirmation box

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import { View, Text, StyleSheet, Button, TouchableWithoutFeedback, Keyboard, Alert} from 'react-native'
 import Card from '../components/Card'
 import Input from '../components/Input'
+import NumberContainer from '../components/NumberContainer'
 import Colors from '../constants/color'
 
 const StartGameScreen = (props) => {
@@ -27,11 +28,18 @@ const StartGameScreen = (props) => {
         setConfirmed(true)
         setSelectedNumber(parseInt(chosenNumber))
         setEnteredValue('')
+        Keyboard.dismiss()
     }
     let confirmedOutput
 
     if(confirmed) {
-        confirmedOutput = <Text>Chosen Number : {selectedNumber}</Text>
+        confirmedOutput = (
+            <Card style={styles.summaryContainer}>
+                <Text>You selected</Text>
+                <NumberContainer>{selectedNumber}</NumberContainer>
+                <Button title="Start Game" />
+            </Card>
+        )
     }
 
     return (
@@ -97,6 +105,11 @@ const styles = StyleSheet.create({
     input: {
         width: 50,
         textAlign: 'center'
+    },
+    summaryContainer: {
+        marginTop: 20,
+        alignItems: 'center',
+
     }
 })
 
