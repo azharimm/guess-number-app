3ae1e3fe866518c4d8a4e69866c939316cb69947
Azhari Muhammad M <azhari.marzan@gmail.com>
2020-03-04 19:37:32 +0700

    + image game over screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import {View, Text, StyleSheet, Button} from 'react-native'
+import {View, StyleSheet, Button, Image} from 'react-native'
+import BodyText from '../components/BodyText'
+import TitleText from '../components/TitleText'
 
 const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>The Game is over!</Text>
-            <Text>Number of rounds: {props.roundNumber}</Text>
-            <Text>Number was: {props.userNumber}</Text>
+            <TitleText>The Game is over!</TitleText>
+            <View style={styles.imageContainer}>
+                <Image
+                    // source={require('../assets/success.png')}
+                    source={{uri: 'https://www.geeky-gadgets.com/wp-content/uploads/2010/10/Everest-Summit.jpg'}}
+                    style={styles.image}
+                    resizeMode="cover" />
+            </View>
+            <BodyText>Number of rounds: {props.roundNumber}</BodyText>
+            <BodyText>Number was: {props.userNumber}</BodyText>
             <Button onPress={props.onRestart} title="NEW GAME" />
         </View>
     )
@@ -17,6 +26,19 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3, 
+        borderColor: 'black',
+        overflow: 'hidden',
+        marginVertical: 30
+    },
+    image: {
+        width: '100%',
+        height: '100%'
     }
 })
 
